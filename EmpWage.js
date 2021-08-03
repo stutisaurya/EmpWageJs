41e0b421e21cbdd173689b1f5ab85ead22033f98
stutisaurya <stuti.saurya@gmail.com>
2021-08-04 00:46:24 +0530

    Added: Calculate EmpWage with condition

--- EmpWage.js
+++ EmpWage.js
@@ -4,6 +4,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HOURS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck)
 {
@@ -19,11 +20,14 @@ switch (empCheck)
         return 0;
 }
 }
+
 let totalEmpHrs=0;
-for (let day=0; day < NUM_OF_WORKING_DAYS; day++)
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random()*3);
     totalEmpHrs= totalEmpHrs + getWorkingHours(empCheck);
 }
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Hours: " +totalEmpHrs +" "+"Employee_Wage: " + empWage);
+console.log("Total Days: "+ totalWorkingDays +" "+"Total Hours: " +totalEmpHrs +" "+"Employee_Wage: " + empWage);
