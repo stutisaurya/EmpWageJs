6d0e82f08ee924dafcabd7910cda5520e52c5e08
stutisaurya <stuti.saurya@gmail.com>
2021-08-04 00:18:32 +0530

    Added: Present or Absent

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,10 @@
-
+const IS_ABSENT = 0 
+let empCheck = Math.floor(Math.random() * 2);
+ if (empCheck ==  IS_ABSENT) 
+ { 
+     console.log("Employee is Absent"); 
+     return;
+  } 
+  else {
+       console.log("Employee is PRESENT");
+  }
