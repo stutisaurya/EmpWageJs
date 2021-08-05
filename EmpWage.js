27037e15a53d590559b4f89bb730e64b3a2e8ac5
stutisaurya <stuti.saurya@gmail.com>
2021-08-05 16:03:33 +0530

    Added:Daily Wage And Daily Hour

--- EmpWage.js
+++ EmpWage.js
@@ -8,14 +8,30 @@ let totalWorkingDays = 0;
 function totalWages(totalWage, dailyWage) {
     return totalWage + dailyWage;
 }
-while ((totalWorkingDays < WORKING_DAYS_IN_MONTH) && empHours <= MAX_HOURS_IN_MONTH) {
-    totalWorkingDays++;
-    let employeeCheck = Math.floor(Math.random() * 10 % 3);
-    let empHours = getWorkingHours(employeeCheck);
-    totalEmpHours += empHours;
-    empDailyWageArr.push(calculateDailyWage(empHours));
-    empDailyHrsMap.set(totalWorkingDays, empHours);
-    empDailyWageMap.set(totalWorkingDays, calculateDailyWage(empHours));
-}
-console.log(empDailyWageMap);
-console.log('UC8-A Emp Wage Map totalHours: ' + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+	const findTotal = (totalVal, dailyVal) => {
+		return totalVal + dailyVal;
+	}
+
+	let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+	let totalSalary = empDailyWageArr
+		.filter(dailyWage => dailyWage > 0)
+		.reduce(findTotal, 0);
+	console.log('UC-9 Employee Wage with Arrow. : ' + 'Total Hours: ' + totalHours + ' Total Wages: ' + totalSalary);
+
+	let nonWorkingDays = new Array();
+	let partWorkingDays = new Array();
+	let fullWorkingDays = new Array();
+	empDailyHrsMap.forEach((value, key, map) => {
+		if (value == 8) {
+			fullWorkingDays.push(key);
+		} else if (value == 4) {
+			partWorkingDays.push(key);
+		} else {
+			nonWorkingDays.push(key);
+		}
+	});
+
+	console.log(' FullTime Working Days: ' + fullWorkingDays);
+	console.log(' PartTime Working Days: ' + partWorkingDays);
+	console.log(' Non Working Days: ' + nonWorkingDays);
+
